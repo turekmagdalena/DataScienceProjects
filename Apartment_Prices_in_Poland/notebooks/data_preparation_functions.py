b2d51f639431e6802e5e92f063c83211e6500b94
turekmagdalena <turek.magdalena.146@gmail.com>
2025-01-28 21:09:47 +0100

    Create model for all cities and start creating prediction app

--- Apartment_Prices_in_Poland/notebooks/data_preparation_functions.py
+++ Apartment_Prices_in_Poland/notebooks/data_preparation_functions.py
@@ -8,12 +8,10 @@ Original file is located at
 """
 
 import pandas as pd
+from scipy.stats.mstats import winsorize
+from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
-def filter_by_city(path, city):
-  df = pd.read_csv(path)
-  df = df[df['city'] == city]
-
-  return df
 
 # HELPER FUNCTIONS
 
@@ -75,29 +73,152 @@ def fill_na_in_type(df):
   return df
 
 
-#  MAIN FUNCTION
+def split_into_bins(df, columns):
+  max_column = df[columns].max().idxmax()
+  max_val = df[max_column].max()
 
-def prepare_data(dataframe):
+  bins = np.linspace(0, max_val, 11)
 
-  # Categorize location
-  df = categorize_location(dataframe)
-  # Create a column for price per square meter
+  for col in columns:
+    df[f'{col}_binned'] = pd.cut(df[col], bins=bins, labels=False, include_lowest=True)
+
+  df = df.drop(columns=columns)
+
+  return df, bins.tolist()
+
+
+def encode_city_column(df, city_column):
+    # Initialize OneHotEncoder for city
+    ohe_city = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+    encoded_city_array = ohe_city.fit_transform(df[[city_column]])
+
+    # Get column names for the encoded features of 'city'
+    encoded_city_feature_names = ohe_city.get_feature_names_out([city_column])
+
+    # Create a DataFrame with the encoded columns for 'city'
+    encoded_city_df = pd.DataFrame(encoded_city_array, columns=encoded_city_feature_names, index=df.index)
+
+    # Concatenate the encoded columns with the original DataFrame (drop the original 'city' column)
+    df = pd.concat([df.drop(columns=[city_column]), encoded_city_df], axis=1)
+
+    return df, ohe_city
+
+def encode_cat_columns(df,cat_columns):
+
+    ohe_cat = OneHotEncoder(drop='first', sparse_output=False, handle_unknown='ignore')
+    encoded_other_array = ohe_cat.fit_transform(df[cat_columns])
+
+    # Get column names for the encoded features of other columns
+    encoded_other_feature_names = ohe_cat.get_feature_names_out(cat_columns)
+
+    # Create a DataFrame with the encoded columns for other categorical columns
+    encoded_other_df = pd.DataFrame(encoded_other_array, columns=encoded_other_feature_names, index=df.index)
+
+    # Concatenate the encoded columns with the original DataFrame (drop the original categorical columns)
+    df = pd.concat([df.drop(columns=cat_columns), encoded_other_df], axis=1)
+
+    return df, ohe_cat
+
+
+
+#  MAIN FUNCTIONS
+
+def prepare_data_per_city(df, city):
+
+  #FILTER BY CITY
+
+  df = df[df['city'] == city]
+
+  # REMOVE FEATURES
+
+  df = df.drop(columns=['id', 'ownership',  'rooms', 'hasSecurity', 'hasStorageRoom', 'buildingMaterial', 'schoolDistance', 'restaurantDistance', 'postOfficeDistance'])
+
+  # Add price_per_m2 column
   df['price_per_m2'] = (df['price'] / df['squareMeters']).round(0)
 
   # FILL MISSING VALUES
+
   df = fill_na_in_type(df)
 
   df["buildYear"] = df.groupby("type")["buildYear"].transform(
     lambda x: x.fillna(round(x.median())))
 
-  df['buildingMaterial'] = df['buildingMaterial'].fillna('other')
+  df['condition'] = df['condition'].fillna('standard')
 
   df['floorCount'] = df['floorCount'].fillna(round(df['floorCount'].median()))
-
-  df['condition'] = df['condition'].fillna('standard')
 
   df['floor'] = df.groupby('floorCount')['floor'].transform(
     lambda x: x.fillna(x.mode().iloc[0] if not x.mode().empty else 1))
 
+  feature_columns = [col for col in df.columns if 'has' in col]
+
+  for col in feature_columns:
+    df[col] = df[col].fillna(df[col].mode()[0])
+
+  distance_columns = [col for col in df.columns if 'Distance' in col]
+
+  for col in distance_columns:
+      df[col] = df[col].fillna(df[col].mean())
+
+
+  # HANDLE OUTLIERS
+
+  lower_limit = df['buildYear'].le(1919).mean()
+  df['buildYear'] = winsorize(df['buildYear'], limits=(lower_limit, 0))
+
+  df['squareMeters'] = winsorize(df['squareMeters'], limits=(0, 0.05))
+
+  df['poiCount'] = winsorize(df['poiCount'], limits=(0, 0.1))
+
+  df['price_per_m2'] = winsorize(df['price_per_m2'], limits=(0, 0.05))
+  df['price_per_m2_log'] = np.log(df['price_per_m2'])
 
   return df
+
+
+
+
+def prepare_data_whole_dataset(df):
+
+
+  # REMOVE FEATURES
+
+  df = df.drop(columns=['price', 'price_per_m2', 'locationCategory', 'floorCount'])
+
+
+  # SPLIT DATA INTO BINS
+
+  distance_columns = [col for col in df.columns if 'Distance' in col]
+
+  distance_columns.remove('centreDistance')
+
+  df, distance_bins = split_into_bins(df, distance_columns)
+
+  df['centreDistance'] = winsorize(df['centreDistance'], limits=(0, 0.05))
+
+  df, centre_distance_bins = split_into_bins(df, ['centreDistance'])
+
+  bins_to_save = {
+    "distance_bins": distance_bins,
+    "centre_distance_bins": centre_distance_bins
+  }
+
+  df = df.drop_duplicates()
+
+  # ENCODE CATEGORICAL COLUMNS
+
+  df, ohe_city = encode_city_column(df, 'city')
+
+  cat_columns = df.select_dtypes('object').columns
+
+  df, ohe_cat = encode_cat_columns(df, cat_columns)
+
+  ohe_dict = {
+        'city_encoder': ohe_city,
+        'cat_column_encoder': ohe_cat
+    }
+
+
+
+
+  return df, bins_to_save, ohe_dict
